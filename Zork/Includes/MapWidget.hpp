6949be086e0c7cb1895d26e5849134c796f283a9
Josh McGiff <jbmcgiff@gmail.com>
2021-03-24 16:07:57 +0000

    Updated Map generation

--- Zork/Includes/MapWidget.hpp
+++ Zork/Includes/MapWidget.hpp
@@ -10,12 +10,17 @@ namespace Ui {
 class MapWidget : public QWidget 
 {
     Q_OBJECT
-
+private:
+QRect playerRect;
+int playerStartX;
+int playerStartY;
 public:
     MapWidget(std::shared_ptr<Game::Zork> zorkInstance, QWidget* parent = nullptr): zork(zorkInstance), QWidget(parent) {}
 
     virtual void paintEvent(QPaintEvent*) override;
     std::shared_ptr<Game::Zork> zork;
+    void MovePlayerNorth();
+
 };
 
 } //namespace Ui
--- Zork/Sources/MapWidget.cpp
+++ Zork/Sources/MapWidget.cpp
@@ -7,14 +7,49 @@ namespace Ui {
 using namespace Game;
 
 void MapWidget::paintEvent(QPaintEvent*) {
+    playerStartX = (START_COL*75)+25;
+    playerStartY = (START_ROW*75)+25;
+    QPixmap pixmap = QPixmap(600,300);
+    pixmap = pixmap.scaled(width(), height(),Qt::KeepAspectRatio,Qt::SmoothTransformation);
+    
     QPainter p(this);
     p.setPen(QPen(Qt::blue, 3));
     int n = 8;
-    p.drawRect(10, 10, 20, 20);
-
     Game::World* world = zork->getCurrentWorld();
-    for(int i = 0; i <)
-    world->roomArray[0][0] = new Room();
+        for(int i = 0; i < ROW_COUNT; i++){
+            for(int j = 0; j < COL_COUNT; j++){
+                if(world->roomArray[i][j] != nullptr){
+                    p.drawRect(10 + (j*75), 10 + (i*75), 50, 50);
+                }
+                if ((i+1 < ROW_COUNT) && (world->roomArray[i][j] != nullptr) && (world->roomArray[i+1][j]) != nullptr) { //North
+                    p.drawLine  (10 + (j*75) +25, //x1
+                                10 + (i*75)+ 50, //y1
+                                10 + (j*75) + 25, //x2
+                                10 + ((i+1)*75)); //y2
+                }
+                if ((j+1 < ROW_COUNT) && (world->roomArray[i][j] != nullptr) && (world->roomArray[i][j+1]) != nullptr) { //North
+                    p.drawLine  (10 + (j*75) + 50, //x1
+                                10 + ((i*75) + 25), //y1
+                                10 + ((j+1)*75), //x2
+                                10 + ((i*75) + 25)); //y2
+                }
+                
+            }
+        }
+            p.setPen(QPen(Qt::red, 3));
+           QRect test = QRect(QPoint(playerStartX, playerStartY), QSize(20, 20));
+            p.fillRect(test, Qt::red);
+            MovePlayerNorth();
+            playerRect = QRect(QPoint(playerStartX, playerStartY), QSize(20, 20));
+            p.drawRect(playerRect);
+            p.eraseRect(test);
+    }
+
+void MapWidget::MovePlayerNorth(){
+    this->playerStartY -= 75;
 }
 
+
+    //world->roomArray[0][0] = new Room();
 }
+
